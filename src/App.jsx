32a973a1cfe6f0dd1fee8639fabe76f9d0e5f595
AzamatMurodjonov <murodjonovazamat07@gmail.com>
2025-03-02 14:54:47 +0500

    image was added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
       <h2>Azamat + Sevara = Love</h2>
       <h5>muhamadali + Maryam = Love</h5>
       <h6>Muhammadali + Brawlstarts = Love</h6>
+      <img src="./src/efotbal-No1.jpg" alt="" />
+
     </>
   );
 }
