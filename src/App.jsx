79b67e055025b3c0b424d23c16982bcd946acdf5
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-03-02 15:15:54 +0500

    asvasdvasvd

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <img className="img1" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" alt="" />
       <img src="./src/efotbal-No1.jpg" alt="" />
       <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.quora.com%2FIf-current-Sung-Jin-Woo-from-the-Solo-Leveling-webtoon-suddenly-appeared-in-the-universe-of-One-Punch-Man-anime-how-would-everyone-react-to-him-and-his-powers-and-abilities&psig=AOvVaw2omSYQYE60qQqzKpO-BVVx&ust=1740995630207000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiao6SQ64sDFQAAAAAdAAAAABAE" alt="" />
-
       <u>Lorem ipsum dolor sit.</u>
       <i>Lorem ipsum dolor sit.</i>
     </>
