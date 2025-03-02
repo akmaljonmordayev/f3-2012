d7f0180e6349b30baf04b3301e73724dc1d6bd5a
abdullohabdumalikov <a37666680@gmail.com>
2025-03-02 14:40:40 +0500

    h3 added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
     <>
       <h1>Lorem, ipsum dolor.</h1>
       <h2>Azamat + Sevara = Love</h2>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        quasi, deleniti est sed corrupti harum vero quas temporibus officiis
+        omnis numquam possimus eveniet quod ut eum id ipsa officia minus!
+      </h3>
     </>
   );
 }
